f9e0eaabb81588b321bce539927fdfcac560d96f
Romane <romane71193@gmail.com>
2018-07-13 12:46:53 -0400

    added password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token,:reset_token
  before_save   :downcase_email
  before_create :create_activation_digest
    validates :name, presence: true, length: { maximum: 50 }
@@ -9,6 +9,19 @@ class User < ApplicationRecord
      uniqueness: { case_sensitive: false }
       has_secure_password
       validates :password, presence: true, length: { minimum: 5 }, allow_nil: true
+
+
+
+      def create_reset_digest
+         self.reset_token = User.new_token
+         # update_attribute(:reset_digest,  User.digest(reset_token))
+         # update_attribute(:reset_sent_at, Time.zone.now)
+         update_columns(reset_digest:  User.digest(reset_token), reset_sent_at: Time.zone.now)
+      end
+
+      def send_password_reset_email
+      UserMailer.password_reset(self).deliver_now
+      end
 
 
 
@@ -37,6 +50,21 @@ end
 def forget
     update_attribute(:remember_digest, nil)
 end
+def activate
+  update_columns(activated:true,activated_at:Time.zone.now)
+# update_attribute(:activated,    true)
+# update_attribute(:activated_at, Time.zone.now)
+end
+
+
+def send_activation_email
+   UserMailer.account_activation(self).deliver_now
+end
+def password_reset_expired?
+   reset_sent_at < 2.hours.ago
+end
+
+private
 # Converts email to all lower-case.
     def downcase_email
      email.downcase!
@@ -47,15 +75,5 @@ end
       self.activation_token  = User.new_token
       self.activation_digest = User.digest(activation_token)
     end
-    def activate
-      update_columns(activated:true,activated_at:Time.zone.now)
-   # update_attribute(:activated,    true)
-   # update_attribute(:activated_at, Time.zone.now)
-    end
-
-    def send_activation_email
-       UserMailer.account_activation(self).deliver_now
-    end
-
 
 end
